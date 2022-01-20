6efe50fec674199fc9063e61621907566e74e398
John Shea <sheajohnh@gmail.com>
2022-01-20 00:02:18 -0500

    Create class DocxIngestor

---
+++ src/QuoteEngine/DocxIngestor.py
@@ -0,0 +1,26 @@
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+from typing import List
+from docx import Document
+
+class DocxIngestor(IngestorInterface):
+    allowed_extensions = ['docx']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception("cannot ingest exception")
+
+        quotes = []
+
+        doc = Document(path)
+
+        for paragraph in doc.paragraphs:
+            text = paragraph.text.strip()
+            if len(text) > 0:
+                data = text.split('-')
+                body, author = data[0].strip('" '), data[1].strip()
+                new_quote = QuoteModel(body, author)
+                quotes.append(new_quote)
+
+        return quotes
