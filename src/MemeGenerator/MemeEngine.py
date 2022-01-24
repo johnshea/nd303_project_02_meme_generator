87beb4cf1b026d42837a23426193363aa7c55303
John Shea <sheajohnh@gmail.com>
2022-01-23 22:06:36 -0500

    Class MemeEngine - refactor and code cleanup

--- src/MemeGenerator/MemeEngine.py
+++ src/MemeGenerator/MemeEngine.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, UnidentifiedImageError
 import random
 import os
 
@@ -8,35 +8,50 @@ class MemeEngine:
       self.output_dir = output_dir
 
   def make_meme(self, img_path, text, author, width=500) -> str: #generated image path
-      im = Image.open(img_path)
-      image_width, image_height = im.size
-      new_width = min(width, 500)
-      new_height = int((new_width / image_width) * image_height)
 
-      out = im.resize((new_width, new_height))
+      try:
+          im = Image.open(img_path)
+      except FileNotFoundError:
+          raise FileNotFoundError(f'File "{img_path}" was not found') from None
+      except UnidentifiedImageError:
+          raise UnidentifiedImageError(f"Unknown image type") from None
 
-      quote = self._formatQuote(text, author)
+      out = im.resize(self._scale_image_dimensions(width, im.width, im.height))
+
+      quote = self._format_quote(text, author)
 
       draw = ImageDraw.Draw(out)
 
-      text_anchor_pos = self._randomlyPositionText(out, draw, quote, image_width, image_height)
+      text_anchor_pos = self._randomly_position_text(out, draw, quote)
 
       draw.text(text_anchor_pos, quote, fill=(255,255,255,255))
 
       outfile = os.path.join(self.output_dir, f'{random.randint(1,10000000)}.png')
-      out.save(outfile)
+
+      try:
+        out.save(outfile)
+      except OSError:
+        raise OSError(f'There was a problem saving file "{outfile}"')
 
       return outfile
 
-  def _formatQuote(self, body: str, author: str) -> str:
+  def _scale_image_dimensions(self, desired_width: int, image_width: int, image_height: int) -> tuple[int, int]:
+      new_width = min(desired_width, 500)
+      new_height = int((new_width / image_width) * image_height)
+      return new_width, new_height
+
+  def _format_quote(self, body: str, author: str) -> str:
       return f'"{body}" - {author}'
 
-  def _randomlyPositionText(self, image: Image, draw: ImageDraw, quote: str, width: int, height: int) -> tuple[int, int]:
+  def _randomly_position_text(self, image: Image, draw: ImageDraw, quote: str) -> tuple[int, int]:
       text_width, text_height = draw.textsize(quote)
 
+      upper_bound_width = max(0, image.width - text_width)
+      upper_bound_height = max(0, image.height - text_height)
+
       text_anchor_pos = (
-        random.randint(0, image.width - text_width),
-        random.randint(0, image.height - text_height)
+          random.randint(0, upper_bound_width),
+          random.randint(0, upper_bound_height)
       )
 
       return text_anchor_pos
